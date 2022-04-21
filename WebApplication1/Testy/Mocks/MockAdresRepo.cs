92e434f04f9d5800f132a01813ef928ee192a408
michheb <michheb695@student.polsl.pl>
2022-04-21 20:51:27 +0200

    test?

---
+++ WebApplication1/Testy/Mocks/MockAdresRepo.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace Testy.Mocks
+{
+    public class MockAdresRepo: Mock<IAdresRepo>
+    {
+        public MockAdresRepo mockGetAdres(IEnumerable<Adres> adres)
+        {
+            Setup(x => x.GetAdres(It.IsAny<>()));
+        }
+
+    }
+}
---
+++ WebApplication1/Testy/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System;
+using Xunit;
+
+namespace Testy
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void DummyExample()
+        {
+            
+        }
+    }
+}
