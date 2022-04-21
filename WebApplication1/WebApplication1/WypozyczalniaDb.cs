f43b5ddc3a1b47f4fcf9c0d50ade3c152d35e9ea
MicWalczak <michwal323@student.polsl.pl>
2022-04-21 20:07:38 +0200

    dbconnect

---
+++ WebApplication1/WebApplication1/WypozyczalniaDb.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class WypozyczalniaDb : DbContext
+    {
+        public DbSet<Pojazd> Pojazd { get; set; }
+        public DbSet<Adres> Adres { get; set; }
+        public DbSet<Pracownicy> Pracownicy { get; set; }
+        public DbSet<Klienci> Klienci { get; set; }
+        public DbSet<Zlecenie> Zlecenie { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                  .UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=WypozyczalniaDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
